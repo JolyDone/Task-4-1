313e38a9420918933dc306b3da57f474c441ba94
JolyDone <twilightclimb@gmail.com>
2023-07-07 13:21:48 +0300

    Жаль добряка

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    @Autowired
+    private Egg6 e;
+
+    public Duck5(Egg6 e) {
+        this.e = e;
+    }
+
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + e.toString();
     }
 }
